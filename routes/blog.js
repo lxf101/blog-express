430184f30446686a459722f910541f9e3f1f8599
lxf101 <lixinfang101@126.com>
2025-02-19 17:12:54 +0800

    modify blog router

--- routes/blog.js
+++ routes/blog.js
@@ -1,11 +1,36 @@
 var express = require('express');
 var router = express.Router();
+const {getList, newBlog, updateBlog, deleteBlog, getDetail} = require('../controller/blog');
+const {SuccessModel, ErrorModel} = require('../model/resModel')
+const loginCheck = require('../middleware/loginCheck')
 
-router.get('/list', function(req, res, next) {
-  res.json({
-    errno: 0,
-    data: [1, 2, 3, 4]
-  })
+router.get('/list', (req, res, next) => {
+    let author = req.query.author || '';
+    let keyword = req.query.keyword || '';
+
+    if(req.query.isadmin){
+        if(req.session.username == null){
+            // not login
+            res.json(new ErrorModel('not login'))
+            return 
+        }
+        // force to search self blog
+        author = req.session.username;
+    }
+    
+    const result = getList(author, keyword);
+    return result.then(listData => {
+        res.json(new SuccessModel(listData))
+    });
 });
+
+router.get('/detail', (req, res, next) => {
+    const result = getDetail(req.query.id);
+    return result.then(data => {
+        res.json(new SuccessModel(data));
+    })
+});
+
+
 
 module.exports = router;
